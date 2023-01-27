452b63dbd758960ca61d2a64bcfea84b0da86f40
imranhacks59 <imranhacks59@gmail.com>
2023-01-27 13:22:09 +0530

    bug fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,9 +14,7 @@ const Navbar = () => {
             <li className="links__link">
               <Link to="/">HOME</Link>
             </li>
-            <li className="links__link">
-              <Link to="/about">ABOUT</Link>
-            </li>
+            
           </ul>
         </nav>
       </div>
